d2399ce763a64c2b1f1481ee29f4a69f9e61867e
3colorr <3colorr@gmail.com>
2022-04-24 14:59:26 +0900

    Add project files

---
+++ Sources/SomethingLikeSwiftUIParts/SomethingLikeSwiftUIParts.swift
@@ -0,0 +1,6 @@
+public struct SomethingLikeSwiftUIParts {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SomethingLikeSwiftUIPartsTests/SomethingLikeSwiftUIPartsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SomethingLikeSwiftUIParts
+
+final class SomethingLikeSwiftUIPartsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SomethingLikeSwiftUIParts().text, "Hello, World!")
+    }
+}
