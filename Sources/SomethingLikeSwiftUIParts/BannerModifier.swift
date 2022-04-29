56b616b530d1508d2a476301acf013316efd5f98
3colorr <3colorr@gmail.com>
2022-04-29 22:39:51 +0900

    Add an banner view modifier file

---
+++ Sources/SomethingLikeSwiftUIParts/BannerModifier.swift
@@ -0,0 +1,53 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by 3colorr on 2022/04/29.
+//
+
+import SwiftUI
+
+struct BannerModifier: ViewModifier {
+    @Binding var isPresentView: Bool
+    let message: Text
+    let shadowRadius: CGFloat
+    let type: Animation
+    let direction: Edge
+    let duration: TimeInterval
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            content
+            
+            if isPresentView {
+                withAnimation(type) {
+                    VStack {
+                        if direction == .bottom {
+                            Spacer()
+                        }
+                        
+                        message
+                            .font(.footnote)
+                            .bold()
+                            .padding(.vertical, 20)
+                            .padding(.horizontal, 40)
+                        
+                        if direction == .top {
+                            Spacer()
+                        }
+                    }
+                    .padding()
+                    .shadow(radius: shadowRadius)
+                    .transition(AnyTransition.move(edge: direction))
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
+                            withAnimation(.linear) {
+                                isPresentView.toggle()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
